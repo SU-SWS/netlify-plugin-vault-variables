d61941ce9c797bb6bd0226cc6a1dce717d15385a
Shea McKinney <sheamck@stanford.edu>
2024-01-15 15:59:32 -0800

    Debug for edge

--- src/index.js
+++ src/index.js
@@ -83,9 +83,9 @@ module.exports = {
       // EDGE FUNCTIONS
       // Ensure that the .env file we are writing gets bundled with the func.
       if (!Array.isArray(netlifyConfig?.edge_functions?.['*']?.included_files)) {
-        netlifyConfig.functions['*'].included_files = [];
+        netlifyConfig.edge_functions['*'].included_files = [];
       }
-      netlifyConfig.functions['*'].included_files.push('.env');
+      netlifyConfig.edge_functions['*'].included_files.push('.env');
     }
 
     // Contextualize the secrets.
