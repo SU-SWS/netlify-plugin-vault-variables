c06760ff5920858843c23e54aa4e6dc55b80f08b
Shea McKinney <sheamck@stanford.edu>
2025-05-01 10:54:42 -0700

    Allow for setting context var in to .env

--- src/index.js
+++ src/index.js
@@ -79,11 +79,13 @@ module.exports = {
     secrets = replaceContextualVars(secrets);
 
     // Store the secrets to write to the .env file.
+    const secretsToWrite = [];
+
     // Add the context var so we can access it during runtime using dotenv.
-    console.log("Adding CONTEXT to the .env file", process.env.CONTEXT);
-    const secretsToWrite = [
-      `CONTEXT=${process.env.CONTEXT}`
-    ];
+    if (inputs.addContext === 'true' || inputs.addContext === true) {
+      console.log('Adding CONTEXT to the .env file', process.env.CONTEXT);
+      secretsToWrite.push(`CONTEXT=${process.env.CONTEXT}`);
+    }
 
     // Create an array of things to write to the env file.
     Object.keys(secrets).forEach((key) => {
